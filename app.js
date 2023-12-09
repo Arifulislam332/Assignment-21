e99cb615a6ecf45393d9bc1a2a1f1748326b7737
Arifulislam332 <ayatullaharif332@gmail.com>
2023-12-09 19:33:11 +0600

    Arif

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 "use strict";
 
-/* const information1 = {
+const information1 = {
   name: "Mariya",
   add: "Puran dhaka-1213",
   age: 22,
@@ -15,8 +15,8 @@ information2.age = "18+";
 const arr = ["a", "b", "c", "d"];
 const arr2 = arr;
 
-// console.log(arr);
-// console.log(arr2);
+console.log(arr);
+console.log(arr2);
 
 arr2.push(100);
 
@@ -31,13 +31,13 @@ const firstClassFn = (a, b) => {
 
 const otherFn = (a, b, fn) => {
   return fn(a, b);
-}; */
+};
 
-// console.log(otherFn(4, 5, firstClassFn));
+console.log(otherFn(4, 5, firstClassFn));
 
 // higher order function (hof)
 // callback func
-/* 
+
 const stylish = document.querySelector(".stylish");
 
 const callback = () => {
@@ -64,22 +64,22 @@ function doMath(a) {
   };
 }
 
-const essayMath = (a) => (b) => (c) => (d) => a + b + c + d; //lambda calculas */
+const essayMath = (a) => (b) => (c) => (d) => a + b + c + d; //lambda calculas
 
-// console.log(essayMath(10)(5)(4)(9));
+console.log(essayMath(10)(5)(4)(9));
 
 // side effect
-// function lowLable(num1, num2) {
-//   return num1 * num2;
-// }
+function lowLable(num1, num2) {
+  return num1 * num2;
+}
 
-// function higherOdr(num1, num2, low) {
-//   return low(num1, num2);
-// }
+function higherOdr(num1, num2, low) {
+  return low(num1, num2);
+}
 
-// console.log(higherOdr(5, 5, lowLable));
+console.log(higherOdr(5, 5, lowLable));
 
-/* function foodCutter(quantity) {
+function foodCutter(quantity) {
   return quantity * 4;
 }
 
@@ -92,30 +92,30 @@ function foodBlender(quantity, fl) {
   return addFlovour(pieces, fl);
 }
 
-console.log(foodBlender(5, 10)); */
+console.log(foodBlender(5, 10));
 
 // call()
-/* function greet(greeting, message) {
+function greet(greeting, message) {
   return `${greeting}, ${this.name}. ${message}`;
 }
 
 const person = { name: "Joya" };
 
-const text = greet.call(person, "Hi", "How are you?"); */
-// console.log(text);
+const text = greet.call(person, "Hi", "How are you?");
+console.log(text);
 
 // Apply
-/* function greet(greeting, message) {
+function greet(greeting, message) {
   return `${greeting}, ${this.name}. ${message}`;
 }
 
 const person2 = { name: "Lili" };
 
-const text2 = greet.apply(person2, ["Fine", "What's up?"]); */
-// console.log(text2);
+const text2 = greet.apply(person2, ["Fine", "What's up?"]);
+console.log(text2);
 
 // bind
-/* function binding(reciver1) {
+function binding(reciver1) {
   return `${reciver1}, ${this.name}.You are  ${
     2023 - this.birthYear
   } years old.`;
@@ -123,14 +123,14 @@ const text2 = greet.apply(person2, ["Fine", "What's up?"]); */
 
 const info1 = { name: "Akhi akter", birthYear: 2004 };
 const info2 = { name: "Farhad", birthYear: 200 };
-const information = binding.bind(info1); */
+const information = binding.bind(info1);
 
-// console.log(information("Hey!"));
+console.log(information("Hey!"));
 
 // IIFE
-/* (function () {
+(function () {
   console.log("hiii");
-})(); */
+})();
 
 // closures
 function closuresFuc() {
